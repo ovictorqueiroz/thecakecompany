2ba8d61db885eb2bab67a0b0b31169a07f923f6b
ovictorqueiroz <victorbr442@gmail.com>
2025-10-02 21:39:53 -0300

    interligando login e menu

--- app/src/main/java/br/com/etecia/myapp/Login_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Login_Activity.java
@@ -1,6 +1,10 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Login_Activity extends AppCompatActivity {
+    MaterialButton btnLogin;
+    TextInputEditText emailInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,28 @@ public class Login_Activity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, senha;
+                email = emailInput.getText().toString();
+                senha = passwordInput.getText().toString();
+
+                if (email.equals("admin") && senha.equals("admin")){
+                    startActivity( new Intent(getApplicationContext(), MenuPrincipalActivity.class));
+                    finish();
+                }
+
+                else{
+                    Toast.makeText(getApplicationContext(), "Usuário ou Senha Inválidos", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
